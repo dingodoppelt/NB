53942d5ecf8622723789b8da9695990c833cc113
dingodoppelt <62596379+dingodoppelt@users.noreply.github.com>
2022-03-28 01:16:23 +0200

    fix output scaling of varSawOSC

--- src/simpleOSCvarSaw.cpp
+++ src/simpleOSCvarSaw.cpp
@@ -92,7 +92,7 @@ struct SimpleOSCvarSaw : Module {
             osc[i].SetPW(aft / 2.f + 0.5f);
             out += osc[i].Process();
         }
-        outputs[OUTPORT_OUTPUT].setVoltage(out);
+        outputs[OUTPORT_OUTPUT].setVoltage(5.f * out);
 	}
 };
 
