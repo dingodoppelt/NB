ac508e3d46f26a25b3f0011196e155ec188818d1
dingodoppelt <62596379+dingodoppelt@users.noreply.github.com>
2022-04-05 17:28:04 +0200

    fix capitalization of filename for moggle.svg

--- src/moggle.cpp
+++ src/moggle.cpp
@@ -40,7 +40,7 @@ struct Moggle : Module {
 struct MoggleWidget : ModuleWidget {
 	MoggleWidget(Moggle* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/Moggle.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/moggle.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
