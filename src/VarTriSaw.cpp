f6d9f32fb8f13950a57aeff09ff5775d59c6faf7
Nils Brederlow <62596379+dingodoppelt@users.noreply.github.com>
2022-10-16 15:09:03 +0200

    fix indentation in varTriSaw

--- src/VarTriSaw.cpp
+++ src/VarTriSaw.cpp
@@ -21,8 +21,8 @@ struct VarTriSaw : Module {
 		LIGHTS_LEN
 	};
 
-    VariableShapeOscillator SQRosc[4], SAWosc[4];
-    float aft_amt[4] = { .6f, .3f, .7f, .8f };
+	VariableShapeOscillator SQRosc[4], SAWosc[4];
+	float aft_amt[4] = { .6f, .3f, .7f, .8f };
 
 	VarTriSaw() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
@@ -30,30 +30,30 @@ struct VarTriSaw : Module {
 		configInput(AFTIN_INPUT, "aftertouch");
 		configOutput(OUTSAW_OUTPUT, "pwm triSaw");
 		configOutput(OUTSQR_OUTPUT, "pwm square");
-        for (int i = 0; i < 4; i++) {
-            SAWosc[i].Init(APP->engine->getSampleRate());
-            SQRosc[i].Init(APP->engine->getSampleRate());
-            SAWosc[i].SetWaveshape(0);
-            SQRosc[i].SetWaveshape(1);
-        }
+		for (int i = 0; i < 4; i++) {
+			SAWosc[i].Init(APP->engine->getSampleRate());
+			SQRosc[i].Init(APP->engine->getSampleRate());
+			SAWosc[i].SetWaveshape(0);
+			SQRosc[i].SetWaveshape(1);
+		}
 	}
 
 	void process(const ProcessArgs& args) override {
-        int AFTchannels = inputs[AFTIN_INPUT].getChannels();
-        int OCTchannels = inputs[VOCT_INPUT].getChannels();
-        int channels = std::max(AFTchannels, OCTchannels);
-        outputs[OUTSAW_OUTPUT].setChannels(channels);
-        outputs[OUTSQR_OUTPUT].setChannels(channels);
-        for(int i = 0; i < channels; i++) {
-            float freq = dsp::FREQ_C4 * powf(2.f, inputs[VOCT_INPUT].getPolyVoltage(std::min(OCTchannels, i)));
-            float pw = inputs[AFTIN_INPUT].getPolyVoltage(std::min(AFTchannels, i)) / 10.f;
-            SAWosc[i].SetFreq(freq);
-            SAWosc[i].SetPW(pw);
-            SQRosc[i].SetFreq(freq);
-            SQRosc[i].SetPW(pw * aft_amt[i % 4]);
-            outputs[OUTSAW_OUTPUT].setVoltage(SAWosc[i].Process() * 10.f, i);
-            outputs[OUTSQR_OUTPUT].setVoltage(SQRosc[i].Process() * 10.f, i);
-        }
+		int AFTchannels = inputs[AFTIN_INPUT].getChannels();
+		int OCTchannels = inputs[VOCT_INPUT].getChannels();
+		int channels = std::max(AFTchannels, OCTchannels);
+		outputs[OUTSAW_OUTPUT].setChannels(channels);
+		outputs[OUTSQR_OUTPUT].setChannels(channels);
+		for(int i = 0; i < channels; i++) {
+			float freq = dsp::FREQ_C4 * powf(2.f, inputs[VOCT_INPUT].getPolyVoltage(std::min(OCTchannels, i)));
+			float pw = inputs[AFTIN_INPUT].getPolyVoltage(std::min(AFTchannels, i)) / 10.f;
+			SAWosc[i].SetFreq(freq);
+			SAWosc[i].SetPW(pw);
+			SQRosc[i].SetFreq(freq);
+			SQRosc[i].SetPW(pw * aft_amt[i % 4]);
+			outputs[OUTSAW_OUTPUT].setVoltage(SAWosc[i].Process() * 10.f, i);
+			outputs[OUTSQR_OUTPUT].setVoltage(SQRosc[i].Process() * 10.f, i);
+		}
 	}
 };
 
