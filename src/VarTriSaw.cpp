f353767531d1043f4f320ae1df3722c56c57555d
dingodoppelt <62596379+dingodoppelt@users.noreply.github.com>
2022-10-17 09:42:34 +0200

    add polygain input and master gain param to varTriSaw

--- src/VarTriSaw.cpp
+++ src/VarTriSaw.cpp
@@ -6,11 +6,13 @@ using namespace daisysp;
 
 struct VarTriSaw : Module {
 	enum ParamId {
+		GAINPARAM_PARAM,
 		PARAMS_LEN
 	};
 	enum InputId {
 		VOCT_INPUT,
 		AFTIN_INPUT,
+		GAININ_INPUT,
 		INPUTS_LEN
 	};
 	enum OutputId {
@@ -24,12 +26,15 @@ struct VarTriSaw : Module {
 
 	VariableShapeOscillator SQRosc[MAX_POLYPHONY], SAWosc[MAX_POLYPHONY];
 	float aft_amt[MAX_POLYPHONY];
+	float gain = 1.f;
 
 	VarTriSaw() {
 		float pi_halves = std::atan(1) * 2;
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(GAINPARAM_PARAM, 0.f, 1.f, 1.f, "gain");
 		configInput(VOCT_INPUT, "v/oct");
 		configInput(AFTIN_INPUT, "aftertouch");
+		configInput(GAININ_INPUT, "poly gain in");
 		configOutput(OUTSAW_OUTPUT, "pwm triSaw");
 		configOutput(OUTSQR_OUTPUT, "pwm square");
 		for (int i = 0; i < MAX_POLYPHONY; i++) {
@@ -44,18 +49,20 @@ struct VarTriSaw : Module {
 	void process(const ProcessArgs& args) override {
 		int AFTchannels = inputs[AFTIN_INPUT].getChannels();
 		int OCTchannels = inputs[VOCT_INPUT].getChannels();
+		int GAINchannels = inputs[VOCT_INPUT].getChannels();
 		int channels = std::max(AFTchannels, OCTchannels);
 		outputs[OUTSAW_OUTPUT].setChannels(channels);
 		outputs[OUTSQR_OUTPUT].setChannels(channels);
 		for(int i = 0; i < channels; i++) {
 			float freq = dsp::FREQ_C4 * powf(2.f, inputs[VOCT_INPUT].getPolyVoltage(std::min(OCTchannels, i)));
 			float pw = inputs[AFTIN_INPUT].getPolyVoltage(std::min(AFTchannels, i)) / 10.f;
+			gain = inputs[GAININ_INPUT].isConnected() ? inputs[GAININ_INPUT].getPolyVoltage(std::min(GAINchannels, i)) / 10.f : params[GAINPARAM_PARAM].getValue();
 			SAWosc[i].SetFreq(freq);
 			SAWosc[i].SetPW((pw + 1.f) / 2.f);
 			SQRosc[i].SetFreq(freq);
 			SQRosc[i].SetPW( (pw * aft_amt[i] + 1.f) / 2.f );
-			outputs[OUTSAW_OUTPUT].setVoltage(SAWosc[i].Process() * 10.f, i);
-			outputs[OUTSQR_OUTPUT].setVoltage(SQRosc[i].Process() * 10.f, i);
+			outputs[OUTSAW_OUTPUT].setVoltage(SAWosc[i].Process() * gain * 10.f, i);
+			outputs[OUTSQR_OUTPUT].setVoltage(SQRosc[i].Process() * gain * 10.f, i);
 		}
 	}
 };
@@ -71,11 +78,14 @@ struct VarTriSawWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.32, 35.391)), module, VarTriSaw::VOCT_INPUT));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.32, 64.663)), module, VarTriSaw::AFTIN_INPUT));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(25.176, 78.609)), module, VarTriSaw::GAINPARAM_PARAM));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.914, 101.817)), module, VarTriSaw::OUTSAW_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(30.469, 101.656)), module, VarTriSaw::OUTSQR_OUTPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.345, 35.714)), module, VarTriSaw::VOCT_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(20.345, 57.383)), module, VarTriSaw::AFTIN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.588, 78.609)), module, VarTriSaw::GAININ_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(9.939, 103.269)), module, VarTriSaw::OUTSAW_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(30.494, 103.108)), module, VarTriSaw::OUTSQR_OUTPUT));
 	}
 };
 
